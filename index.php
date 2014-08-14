8fea8dc30a5ece1cdb76596ac6ee68394d9da427
Processoriented <processoriented@gmail.com>
2014-08-13 19:55:52 -0500

    experimenting with twitter

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	</div>
 	<div class="bd">
 		<h3>Tweets</h3>
-		<a class="twitter-timeline" href="https://twitter.com/processoriented/lists/project-management" data-widget-id="499717714047295488">Tweets from https://twitter.com/processoriented/lists/project-management</a>
+		<a class="twitter-timeline" width="680" href="https://twitter.com/processoriented/lists/project-management" data-widget-id="499717714047295488">Tweets from https://twitter.com/processoriented/lists/project-management</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	</div>
 </div>
