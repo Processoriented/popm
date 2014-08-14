2e8908e0902113c5707290727f3e1ac226baf576
Processoriented <processoriented@gmail.com>
2014-08-14 13:55:20 -0500

    Updated with POPM Twitter Feeds
    Created the POPM Twitter Account and updated settings for the new feeds

--- index.php
+++ index.php
@@ -9,20 +9,17 @@
 		<h2>News</h2>
 	</div>
 	<div class="bd">
-		<h3>Status Update</h3>
-		<p>The project is chugging along.  Today I've added icons to my major functions on the site navigation, and I've relocated the sign-in form to the sidebar (although it still exists in it's own php file in case I ever need that back.</p>
+		<div class="column left">
+			<h3>POPM Status Updates</h3>
+			<a class="twitter-timeline" href="https://twitter.com/popm_guru" data-widget-id="499991197088104448" data-chrome="noheader nofooter transparent noscrollbar" >Tweets by @popm_guru</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div class="column right">
+			<h3>Project Management Tweets</h3>
+			<a class="twitter-timeline" href="https://twitter.com/popm_guru/lists/project-management" data-widget-id="499991523392376834" data-chrome="noheader nofooter transparent noscrollbar" >Tweets from https://twitter.com/popm_guru/lists/project-management</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div class="clear"></div>
 	</div>
 </div>
-<div class="block">
-	<div class="hd">
-		<h2>From the Twitterverse</h2>
-	</div>
-	<div class="bd">
-		<h3>Tweets</h3>
-		<a class="twitter-timeline" width="680" href="https://twitter.com/processoriented/lists/project-management" data-widget-id="499717714047295488">Tweets from https://twitter.com/processoriented/lists/project-management</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
-</div>
-
-
 <?php display_rest_of_page(); ?>
